8f20e2a4b107690595a724b2f5e21445904fbd2a
ivmartel <ivmartel@gmail.com>
2024-03-22 19:09:41 +0100

    Fix decoderScripts setting, fixes #11

--- public/wpinit.js
+++ public/wpinit.js
@@ -12,13 +12,11 @@ dwv.wp.init = function () {
   }
   dwv.wp.init_was_called = true;
 
-  var dwvPath = wp.pluginsUrl + '/dicom-support/node_modules/dwv';
+  var dwvPath = wp.pluginsUrl + '/dwv-wordpress/node_modules/dwv';
 
   // image decoders (for web workers)
-  dwv.decoderScripts = {
-    jpeg2000: dwvPath + '/decoders/pdfjs/decode-jpeg2000.js',
-    'jpeg-lossless': dwvPath + '/decoders/rii-mango/decode-jpegloss.js',
-    'jpeg-baseline': dwvPath + '/decoders/pdfjs/decode-jpegbaseline.js',
-    rle: dwvPath + '/decoders/dwv/decode-rle.js',
-  };
+  dwv.decoderScripts.jpeg2000 = dwvPath + '/decoders/pdfjs/decode-jpeg2000.js';
+  dwv.decoderScripts['jpeg-lossless'] = dwvPath + '/decoders/rii-mango/decode-jpegloss.js';
+  dwv.decoderScripts['jpeg-baseline'] = dwvPath + '/decoders/pdfjs/decode-jpegbaseline.js';
+  dwv.decoderScripts.rle = dwvPath + '/decoders/dwv/decode-rle.js';
 };
